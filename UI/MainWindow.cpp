4a37208480270d6ee505094c920e3540f8b29e29
fmckenna <fmckenna@berkeley.edu>
2019-09-23 10:41:56 -0700

    fmk - to MainWindow SimCEnter header and footers, file load and save options, having main read simcenter style sheet for header and footer

--- UI/MainWindow.cpp
+++ UI/MainWindow.cpp
@@ -4,6 +4,12 @@
 #include <QRect>
 #include <QGuiApplication>
 #include <QScreen>
+#include <QVBoxLayout>
+#include <HeaderWidget.h>
+#include <FooterWidget.h>
+#include <QMenu>
+#include <QMenuBar>
+#include <QMessageBox>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent)
@@ -14,9 +20,33 @@ MainWindow::MainWindow(QWidget *parent) :
     int width  = this->width()<int(0.35*rec.width())?int(0.35*rec.width()):this->width();
     this->resize(width, height);
 
-    QWidget *theWidget = new RockOutcrop();
-    this->setCentralWidget(theWidget);
+    QWidget *centralWidget = new QWidget();
+    QVBoxLayout *layout = new QVBoxLayout();
+    centralWidget->setLayout(layout);
 
+    //
+    // add SimCenter Header
+    //
+
+    QString appName("s3hark");
+    HeaderWidget *header = new HeaderWidget();
+    header->setHeadingText(appName);
+    layout->addWidget(header);
+
+    theRockOutcropWidget = new RockOutcrop();
+    //    this->setCentralWidget(theWidget);
+    layout->addWidget(theRockOutcropWidget);
+
+    //
+    // add SimCenter footer
+    //
+
+    FooterWidget *footer = new FooterWidget();
+    layout->addWidget(footer);
+
+    this->setCentralWidget(centralWidget);
+
+    this->createActions();
     /*
     // margins of centralWidget
     centralWidget()->layout()->setContentsMargins(0, 0, 0, 0);
@@ -30,3 +60,190 @@ MainWindow::~MainWindow()
 
 }
 
+
+void MainWindow::createActions() {
+    QMenu *fileMenu = menuBar()->addMenu(tr("&File"));
+
+    QAction *openAction = new QAction(tr("&Open"), this);
+    openAction->setShortcuts(QKeySequence::Open);
+    openAction->setStatusTip(tr("Open an existing file"));
+    connect(openAction, &QAction::triggered, this, &MainWindow::open);
+    fileMenu->addAction(openAction);
+    //fileToolBar->addAction(openAction);
+
+
+    QAction *saveAction = new QAction(tr("&Save"), this);
+    saveAction->setShortcuts(QKeySequence::Save);
+    saveAction->setStatusTip(tr("Save the document to disk"));
+    connect(saveAction, &QAction::triggered, this, &MainWindow::save);
+    fileMenu->addAction(saveAction);
+
+    QAction *saveAsAction = new QAction(tr("&Save As"), this);
+    saveAsAction->setStatusTip(tr("Save the document with new filename to disk"));
+    connect(saveAsAction, &QAction::triggered, this, &MainWindow::saveAs);
+    fileMenu->addAction(saveAsAction);
+
+    /*
+    thePreferences = SimCenterPreferences::getInstance(this);
+    QAction *preferenceAction = new QAction(tr("&Preferences"), this);
+    preferenceAction->setStatusTip(tr("Set application preferences"));
+    connect(preferenceAction, &QAction::triggered, this, &MainWindow::preferences);
+    fileMenu->addAction(preferenceAction);
+    */
+
+    // strangely, this does not appear in menu (at least on a mac)!! ..
+    // does Qt not allow as in tool menu by default?
+    // check for yourself by changing Quit to drivel and it works
+    QAction *exitAction = new QAction(tr("&Quit"), this);
+    connect(exitAction, SIGNAL(triggered()), qApp, SLOT(quit()));
+    exitAction->setStatusTip(tr("Exit the application"));
+    fileMenu->addAction(exitAction);
+
+    QMenu *helpMenu = menuBar()->addMenu(tr("&Help"));
+    /*
+    QAction *versionAct = helpMenu->addAction(tr("&Version"), this, &MainWindow::version);
+    QAction *aboutAct = helpMenu->addAction(tr("&About"), this, &MainWindow::about);
+    QAction *manualAct = helpMenu->addAction(tr("&Manual"), this, &MainWindow::manual);
+    QAction *submitAct = helpMenu->addAction(tr("&Provide Feedback"), this, &MainWindow::submitFeedback);
+    QAction *submitFeature = helpMenu->addAction(tr("&Submit Feature Request"), this, &MainWindow::submitFeatureRequest);
+    QAction *citeAct = helpMenu->addAction(tr("&How to Cite"), this, &MainWindow::cite);
+    QAction *copyrightAct = helpMenu->addAction(tr("&License"), this, &MainWindow::copyright);
+    */
+}
+
+
+bool MainWindow::save()
+{
+    if (currentFile.isEmpty()) {
+        return saveAs();
+    } else {
+        return saveFile(currentFile);
+    }
+}
+
+bool MainWindow::saveAs()
+{
+    //
+    // get filename
+    //
+
+    QFileDialog dialog(this);
+    dialog.setWindowModality(Qt::WindowModal);
+    dialog.setAcceptMode(QFileDialog::AcceptSave);
+    if (dialog.exec() != QDialog::Accepted)
+        return false;
+
+    // and save the file
+    return saveFile(dialog.selectedFiles().first());
+}
+
+void MainWindow::open()
+{
+    QString fileName = QFileDialog::getOpenFileName(this);
+    if (!fileName.isEmpty())
+        loadFile(fileName);
+}
+
+
+void MainWindow::openFile(QString fileName)
+{
+    if (!fileName.isEmpty())
+        loadFile(fileName);
+}
+
+void MainWindow::newFile()
+{
+    // clear old
+   // inputWidget->clear();
+
+    // set currentFile blank
+    setCurrentFile(QString());
+}
+
+
+void MainWindow::setCurrentFile(const QString &fileName)
+{
+    currentFile = fileName;
+    //  setWindowModified(false);
+
+    QString shownName = currentFile;
+    if (currentFile.isEmpty())
+        shownName = "untitled.json";
+
+    setWindowFilePath(shownName);
+}
+
+bool MainWindow::saveFile(const QString &fileName)
+{
+    //
+    // open file
+    //
+
+    QFile file(fileName);
+    if (!file.open(QFile::WriteOnly | QFile::Text)) {
+        QMessageBox::warning(this, tr("Application"),
+                             tr("Cannot write file %1:\n%2.")
+                             .arg(QDir::toNativeSeparators(fileName),
+                                  file.errorString()));
+        return false;
+    }
+
+
+    //
+    // create a json object, fill it in & then use a QJsonDocument
+    // to write the contents of the object to the file in JSON format
+    //
+
+    QJsonObject json;
+    theRockOutcropWidget->outputToJSON(json);
+    QJsonDocument doc(json);
+    file.write(doc.toJson());
+
+    // close file
+    file.close();
+
+    // set current file
+    setCurrentFile(fileName);
+
+    return true;
+}
+
+void MainWindow::loadFile(const QString &fileName)
+{
+    //
+    // open file
+    //
+
+    QFile file(fileName);
+    if (!file.open(QFile::ReadOnly | QFile::Text)) {
+        QMessageBox::warning(this, tr("Application"),
+                             tr("Cannot read file %1:\n%2.")
+                             .arg(QDir::toNativeSeparators(fileName), file.errorString()));
+        return;
+    }
+
+    // validate the document
+    // JsonValidator class already takes a model type param, add additional model types as required
+
+    /*
+    JsonValidator *jval = new JsonValidator();
+    jval->validate(this, BIM, fileName);
+*/
+
+    // place contents of file into json object
+    QString val;
+    val=file.readAll();
+    QJsonDocument doc = QJsonDocument::fromJson(val.toUtf8());
+    QJsonObject jsonObj = doc.object();
+
+    // close file
+    file.close();
+
+    // given the json object, create the C++ objects
+    if ( ! (currentFile.isNull() || currentFile.isEmpty()) ) {
+        // theRockOutcropWidget->clear(); no method
+    }
+    theRockOutcropWidget->inputFromJSON(jsonObj);
+
+    setCurrentFile(fileName);
+}
--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include "UI/MainWindow.h"
 #include <QApplication>
 #include <QChar>
+#include <QFile>
 
 int main(int argc, char *argv[])
 {
@@ -9,6 +10,16 @@ int main(int argc, char *argv[])
     MainWindow m;
     m.setWindowTitle(QString("s") + QChar(0x00B3) + "hark - Site-specific Seismic Hazard Analysis & Research Kit" );
     m.show();
+
+
+    QFile file(":/styleCommon/common_experimental.qss");
+
+    if(file.open(QFile::ReadOnly)) {
+      QString styleSheet = QLatin1String(file.readAll());
+
+      a.setStyleSheet(styleSheet);
+      file.close();
+    }
 
     return a.exec();
 }
