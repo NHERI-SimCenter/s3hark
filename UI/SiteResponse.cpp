b5018bf6ca5ce0c2fe0212acf9e602b1a492a6b9
Charles Wang <c_w@berkeley.edu>
2020-04-05 15:51:01 -0700

    🔫 kill SSSharkThread elegantly; automatically check 2D/3D in SiteResponse;

--- UI/SiteResponse.cpp
+++ UI/SiteResponse.cpp
@@ -5,7 +5,8 @@
 #include <functional>
 #include <sstream>
 #include <string>
-
+#include <nlohmann/json.hpp>
+using json = nlohmann::json;
 
 
 
@@ -16,7 +17,7 @@ OPS_Stream *opserrPtr = &ferr;
 OPS_Stream *opsoutPtr = &sserr;
 
 
-SiteResponse::SiteResponse(std::string configureFile,std::string anaDir,std::string outDir,std::string femLog, std::function<void(double)> callbackFunction ) :
+SiteResponse::SiteResponse(std::string configureFile,std::string anaDir,std::string outDir,std::string femLog, std::function<bool(double)> callbackFunction ) :
     m_configureFile(configureFile),
     m_analysisDir(anaDir),
     m_outputDir(outDir),
@@ -44,6 +45,15 @@ void SiteResponse::init(std::string configureFile,std::string anaDir,std::string
     ferr_true = new FileStream(m_femLog.c_str());
     opserrPtr = ferr_true;
 
+    std::ifstream i(configureFile);
+    json SRT;
+    i >> SRT;
+    std::string thisSimType = SRT["basicSettings"]["simType"];
+
+    is3D = false;
+    if (!thisSimType.compare("3D2D")) is3D = true;
+    if (!thisSimType.compare("3D1D")) is3D = true;
+
     //./siteresponse ../test/siteLayering.loc -bbp ../test/9130326.nwhp.vel.bbp out thisLog
     // read the layering file
     std::string layersFN("/Users/simcenter/Codes/SimCenter/SiteResponseTool/test/siteLayering.loc");
@@ -68,20 +78,42 @@ void SiteResponse::init(std::string configureFile,std::string anaDir,std::string
         model->setOutputDir(bbpOName);
     }
     else {
-        std::string bbpLName = "Log";
-        //std::string motionXFN("/Users/simcenter/Codes/SimCenter/SiteResponseTool/test/RSN766_G02_000_VEL");
-        std::string motionXFN(anaDir+"/Rock-x");//TODO: may not work on windows
-        motionX.setMotion(motionXFN.c_str());
-        bbpOName = "out";
-        model = new SiteResponseModel("2D", &motionX, m_callbackFunction);
-        //model = new SiteResponseModel("2D", &motionX);
-        model->setOutputDir(outDir);
-        model->setAnalysisDir(anaDir);
-        model->setTclOutputDir(outDir);
-        model->setConfigFile(configureFile);
 
 
-        //buildTcl();
+        if (is3D)
+        {
+            // 3D
+            std::string bbpLName = "Log";
+            //std::string motionXFN("/Users/simcenter/Codes/SimCenter/SiteResponseTool/test/RSN766_G02_000_VEL");
+            std::string motionXFN(anaDir+"/Rock-x");//TODO: may not work on windows
+            std::string motionZFN(anaDir+"/Rock-y");//TODO: may not work on windows
+            motionX.setMotion(motionXFN.c_str());
+            motionZ.setMotion(motionZFN.c_str());
+            bbpOName = "out";
+            //model = new SiteResponseModel("3D", &motionX, &motionZ, m_callbackFunction);
+            model = new SiteResponseModel("3D", &motionX, &motionZ);
+            //model = new SiteResponseModel("2D", &motionX);
+            model->setOutputDir(outDir);
+            model->setAnalysisDir(anaDir);
+            model->setTclOutputDir(outDir);
+            model->setConfigFile(configureFile);
+
+            //buildTcl();
+        } else {//2D
+            std::string bbpLName = "Log";
+            //std::string motionXFN("/Users/simcenter/Codes/SimCenter/SiteResponseTool/test/RSN766_G02_000_VEL");
+            std::string motionXFN(anaDir+"/Rock-x");//TODO: may not work on windows
+            motionX.setMotion(motionXFN.c_str());
+            bbpOName = "out";
+            model = new SiteResponseModel("2D", &motionX, m_callbackFunction);
+            //model = new SiteResponseModel("2D", &motionX);
+            model->setOutputDir(outDir);
+            model->setAnalysisDir(anaDir);
+            model->setTclOutputDir(outDir);
+            model->setConfigFile(configureFile);
+
+            //buildTcl();
+        }
     }
 
 }
@@ -97,10 +129,30 @@ void SiteResponse::buildTcl3D()
     model->buildEffectiveStressModel3D(runAnalysis);
 }
 
+void SiteResponse::kill()
+{
+    model->setForward(false);
+}
+
+
 int SiteResponse::run()
+{
+    if (is3D) run3D();
+    else run2D();
+}
+int SiteResponse::run2D()
 {
     bool runAnalysis = true;
     if (model->buildEffectiveStressModel2D(runAnalysis)!=100)
+        return -1;
+    else
+        return 1;
+}
+
+int SiteResponse::run3D()
+{
+    bool runAnalysis = true;
+    if (model->buildEffectiveStressModel3D(runAnalysis)!=100)
         return -1;
     else
         return 1;
